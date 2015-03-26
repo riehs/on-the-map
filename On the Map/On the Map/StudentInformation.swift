81f1e94e711196d057f2e1d0f130b5cbdb62066f
Daniel Riehs <riehs@bc.edu>
2015-03-26 17:58:29 -0400

    Committed by Udacity

---
+++ On the Map/On the Map/StudentInformation.swift
@@ -0,0 +1,37 @@
+//
+//  StudentInformation.swift
+//  On the Map
+//
+//  Created by Daniel Riehs on 3/22/15.
+//  Copyright (c) 2015 Daniel Riehs. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInformation {
+    
+    var firstName = ""
+    var lastName = ""
+    var latitude = 0.0
+    var longitude = 0.0
+    var mediaURL = ""
+    
+    //The if statements are necessary because the Parse data may not contain all keys.
+    init(dictionary: [String : AnyObject]) {
+        if let first = dictionary["firstName"] as? String {
+            firstName = first
+        }
+        if let last = dictionary["lastName"] as? String {
+            lastName = last
+        }
+        if let lat = dictionary["latitude"] as? Double {
+            latitude = lat
+        }
+        if let long = dictionary["longitude"] as? Double {
+            longitude = long
+        }
+        if let media = dictionary["mediaURL"] as? String {
+            mediaURL = media
+        }
+    }
+}
