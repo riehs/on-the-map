ec4b3bb4e121cd02f25cc8c855baab65043b2ce9
Daniel Riehs <riehs@bc.edu>
2015-03-26 17:58:29 -0400

    Committed by Udacity

---
+++ On the Map/On the Map/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by Daniel Riehs on 3/22/15.
+//  Copyright (c) 2015 Daniel Riehs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //This label displays user instructions, errors, and a "Connecting..." message.
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!
+
+
+    @IBAction func tapSignUpButton(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "https://www.udacity.com/account/auth#!/signup")!)
+    }
+    
+    
+    //Tapping the loginButton fetches a userKey from Udacity, fetches a firstName and a lastName from Udacity, and then fetches student information from Parse. Only when all three steps are complete is the completeLogin function called to segue to the map.
+    @IBAction func tapLoginButton(sender: AnyObject) {
+        self.loginButton.enabled = false
+        self.errorLabel.text = "Connecting..."
+        UdacityLogin.sharedInstance().loginToUdacity(self.usernameTextField.text, password: self.passwordTextField.text) { (success, errorString) in
+            if success {
+                UdacityLogin.sharedInstance().setFirstNameLastName() { (success, errorString) in
+                    if success {
+                        MapPoints.sharedInstance().fetchData() { (success, errorString) in
+                            if success {
+                                self.completeLogin()
+                            } else {
+                                self.displayError(errorString)
+                            }
+                        }
+                    } else {
+                        self.displayError(errorString)
+                    }
+                }
+            } else {
+                self.displayError(errorString)
+            }
+        }
+    }
+    
+
+    //Complete the login and present the first navigation controller.
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), {
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("StudentsNavigationController") as UINavigationController
+            self.presentViewController(controller, animated: true, completion: nil)
+        })
+    }
+
+    
+    //Display error messages returned from any of the completion handlers.
+    func displayError(errorString: String?) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if let errorString = errorString? {
+                self.errorLabel.text = errorString
+                //The login button in re-enabled so that the user can try again.
+                self.loginButton.enabled = true
+            }
+        })
+    }
+}
