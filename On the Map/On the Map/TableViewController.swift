e50cd727fb72b90e44e04b31d5288d5a486d4752
Daniel Riehs <riehs@bc.edu>
2015-03-26 17:58:29 -0400

    Committed by Udacity

---
+++ On the Map/On the Map/TableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewController.swift
+//  On the Map
+//
+//  Created by Daniel Riehs on 3/22/15.
+//  Copyright (c) 2015 Daniel Riehs. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    
+    //The table is reloaded every time the view appears, but new data will only be available if the MapPoints.fetchData() function has been called.
+    override func viewWillAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    
+    //The data for the table cells is stored in the mapPonts array in the sharedInstace of the MapPoints object.
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellReuseIdentifier = "MapPointTableViewCell"
+        let mapPoint = MapPoints.sharedInstance().mapPoints[indexPath.row]
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier) as UITableViewCell
+        
+        cell.textLabel!.text = mapPoint.firstName + " " + mapPoint.lastName
+        return cell
+    }
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MapPoints.sharedInstance().mapPoints.count
+    }
+
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        UIApplication.sharedApplication().openURL(NSURL(string: MapPoints.sharedInstance().mapPoints[indexPath.row].mediaURL)!)
+    }
+}
