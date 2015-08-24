43a153a58570080d35e4a54179b31670b80423cd
Daniel Riehs <riehs@bc.edu>
2015-08-24 08:14:42 -0400

    Remove unnecessary leading space

--- On the Map/On the Map/StudentInformation.swift
+++ On the Map/On the Map/StudentInformation.swift
@@ -14,22 +14,22 @@ struct StudentInformation {
 	var latitude = 0.0
 	var longitude = 0.0
 	var mediaURL = ""
-	
+
 	//The if statements are necessary because the Parse data may not contain all keys.
 	init(dictionary: [String : AnyObject]) {
-		
+
 		var firstName = ""
 		var lastName = ""
-		
+
 		if let first = dictionary["firstName"] as? String {
 			firstName = first
 		}
 		if let last = dictionary["lastName"] as? String {
 			lastName = last
 		}
-		
+
 		fullName = firstName + " " + lastName
-		
+
 		if let lat = dictionary["latitude"] as? Double {
 			latitude = lat
 		}
