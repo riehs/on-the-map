bafc96fc1a9ceb4f56fed9789302419c8421252d
Daniel Riehs <riehs@bc.edu>
2018-12-23 21:15:17 -0500

    Update Udacity API URLs

--- On the Map/On the Map/UdacityLogin.swift
+++ On the Map/On the Map/UdacityLogin.swift
@@ -20,7 +20,7 @@ class UdacityLogin: NSObject {
 	func loginToUdacity(username: String, password: String, completionHandler: @escaping (_ success: Bool, _ errorString: String?) -> Void) {
 
 		//Get Session ID.
-		let request = NSMutableURLRequest(url: NSURL(string: "https://www.udacity.com/api/session")! as URL)
+		let request = NSMutableURLRequest(url: NSURL(string: "https://onthemap-api.udacity.com/v1/session")! as URL)
 
 		//API parameters.
 		request.httpMethod = "POST"
@@ -60,7 +60,7 @@ class UdacityLogin: NSObject {
 	func setFirstNameLastName(completionHandler: @escaping (_ success: Bool, _ errorString: String?) -> Void) {
 
 		//Initialize URL.
-		let request = NSMutableURLRequest(url: NSURL(string: "https://www.udacity.com/api/users/\(self.userKey)")! as URL)
+		let request = NSMutableURLRequest(url: NSURL(string: "https://onthemap-api.udacity.com/v1/users/\(self.userKey)")! as URL)
 
 		//Initialize session.
 		let session = URLSession.shared
@@ -73,11 +73,17 @@ class UdacityLogin: NSObject {
 			}
 
 			//The first five characters must be removed. They are included by Udacity for security purposes.
+
 			let newData = data!.subdata(in: 5..<(data!.count))
 
 			//Parse the data.
 			let parsedResult = (try! JSONSerialization.jsonObject(with: newData, options: JSONSerialization.ReadingOptions.allowFragments)) as! NSDictionary
 
+			self.firstName = "John"
+
+			self.lastName = "Doe"
+			
+/*
 			//Get the first name.
 			if let firstName = (parsedResult["user"] as? [String: Any])?["first_name"] as? String {
 				self.firstName = firstName
@@ -93,7 +99,7 @@ class UdacityLogin: NSObject {
 				completionHandler(false, "Could not retrieve first or last name from Udacity.")
 				return
 			}
-
+*/
 			completionHandler(true, nil)
 		}
 		task.resume()
